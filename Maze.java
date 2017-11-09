5bbc0df24bb3a7adc0d955a3fe36ebc7d7a16b9d
Luke Salamone <lukesalamone@users.noreply.github.com>
2017-11-08 19:47:16 -0500

    Update Maze.java

--- Maze.java
+++ Maze.java
@@ -32,14 +32,7 @@ public class Maze{
 		setupGrid();
 	}
 
-	/*
-	1. Choose a starting point in the field.
-	2. Randomly choose a wall at that point and carve a passage through to the adjacent cell, 
-		but only if the adjacent cell has not been visited yet. This becomes the new current cell.
-	3. If all adjacent cells have been visited, back up to the last cell that has uncarved walls 
-		and repeat.
-	4. The algorithm ends when the process has backed all the way up to the starting point.
-	*/
+	// draw maze to grid using recursive backtracking
 	public void draw(){
 		// find random starting cell
 		Cell current = randomCell(false);
